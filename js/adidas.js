3b7044ece08a94cdd1b52b32643df0d114dd085f
farhansalman96 <farhankakaofc@gmail.com>
2025-03-30 23:16:12 +0500

    Add files via upload

---
+++ js/adidas.js
@@ -0,0 +1,15 @@
+
+<script>
+    const hamBurger = document.querySelector(".toggle-btn");
+
+hamBurger.addEventListener("click", function () {
+  document.querySelector("#sidebar").classList.toggle("expand")
+});
+</script>
+
+  function change() {
+    document.getElementById("imgcart1").src="images/img1.png"
+}
+function normal() {
+    document.getElementById("imgcart1").src="images/adidas/adidas1.jpeg"
+}
---
+++ js/nike.js
@@ -0,0 +1,15 @@
+{/* <script>
+      function change1() {
+    document.getElementById("nikecart1").src="../images/nike/JORDAN+SPIZIKE+LOW.png"
+}
+function normal1() {
+    document.getElementById("nikecart1").src="../images/nike/JORDAN+SPIZIKE+LOW (1).png"
+}
+function change2() {
+    document.getElementById("nikecart2").src="../images/nike/NIKE+DUNK+LOW+NN(1).jpg"
+}
+function normal2() {
+    document.getElementById("nikecart2").src="../images/nike/NIKE+DUNK+LOW+NN.png"
+}
+
+</script> */}
